9fe33c2358927593602ee70518d030be4e871b9a
EliasFIlho <eliasfilho202@gmail.com>
2025-01-05 13:33:06 -0300

    empty

--- stm32f0/Src/tests/tests.c
+++ stm32f0/Src/tests/tests.c
@@ -71,38 +71,87 @@ void USART_TEST_TRANSMITTE_SINGLE_BYTE(){
 }
 void USART_TEST_TRANSMITTE_BUFFER(){
 	gpio_init(RCC_GPIOA);
-		gpio_config_t tx_pin = {
+	gpio_config_t tx_pin = {
+					.GPIOX = GPIOA,
+					.direction = ALTERNATE_FUNCTION,
+					.pin = 9,
+					.AF = AF1
+			};
+	gpio_config_t rx_pin = {
 						.GPIOX = GPIOA,
 						.direction = ALTERNATE_FUNCTION,
-						.pin = 9,
+						.pin = 10,
 						.AF = AF1
 				};
-		gpio_config_t rx_pin = {
-							.GPIOX = GPIOA,
-							.direction = ALTERNATE_FUNCTION,
-							.pin = 10,
-							.AF = AF1
-					};
-		usart_cfg_t usart = {
-			.baud_rate = 9600,
-			.directions = TRANSMITTER_ONLY,
-			.irq_en_sel = USART_DISABLE_IRQ,
-			.mode = USART_MODE_8_BITS,
-			.rx_pin = &rx_pin,
-			.tx_pin = &tx_pin,
-			.stop_bit_n = STOP_BIT_1,
-			.usart_div = USART_DIV_16,
-		};
-		usart_init(&usart);
-		char *data = "elias\n";
-		while(1){
-			usart_send_buffer(data, 6);
-			delay_ms_sys(500);
-		}
+	usart_cfg_t usart = {
+		.baud_rate = 9600,
+		.directions = TRANSMITTER_ONLY,
+		.irq_en_sel = USART_DISABLE_IRQ,
+		.mode = USART_MODE_8_BITS,
+		.rx_pin = &rx_pin,
+		.tx_pin = &tx_pin,
+		.stop_bit_n = STOP_BIT_1,
+		.usart_div = USART_DIV_16,
+	};
+	usart_init(&usart);
+
+	while(1){
+		usart_send_buffer("testando\n", 9);
+		delay_ms_sys(500);
+	}
 
 }
 
-void GPIO_TEST_POLLING_READ(){}
+void USART_TEST_POLLING_READ(){
+	gpio_init(RCC_GPIOA);
+	gpio_config_t led_pin = {
+					.GPIOX = GPIOA,
+					.direction = OUTPUT,
+					.output_mode = PUSH_PULL,
+					.pupdr = NO_PUPD,
+					.speed = LOW_SPEED,
+					.pin = 4
+			};
+	gpio_cfg_peripheral(&led_pin);
+	gpio_config_t tx_pin = {
+					.GPIOX = GPIOA,
+					.direction = ALTERNATE_FUNCTION,
+					.pin = 9,
+					.AF = AF1
+			};
+	gpio_config_t rx_pin = {
+						.GPIOX = GPIOA,
+						.direction = ALTERNATE_FUNCTION,
+						.pin = 10,
+						.AF = AF1
+				};
+	usart_cfg_t usart = {
+		.baud_rate = 9600,
+		.directions = RECEIVER_ONLY,
+		.irq_en_sel = USART_DISABLE_IRQ,
+		.mode = USART_MODE_8_BITS,
+		.rx_pin = &rx_pin,
+		.tx_pin = &tx_pin,
+		.stop_bit_n = STOP_BIT_1,
+		.usart_div = USART_DIV_16,
+	};
+	usart_init(&usart);
+	uint8_t data;
+	while(1){
+		data = usart_read_byte();
+		if(data == '1'){
+			gpio_set_pin(led_pin.GPIOX, led_pin.pin);
+		}else{
+			gpio_reset_pin(led_pin.GPIOX, led_pin.pin);
+		}
+
+
+	}
+
+
+}
+
+
 void GPIO_TEST_INTERRUPT_READ(){}
 
 void TIMER_TEST_INPUT_CAPTURE(){
